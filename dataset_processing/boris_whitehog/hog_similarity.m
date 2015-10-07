37f393bc060b9403f97e702e71fe3ca6a2466b09
Artsiom Sanakoyeu <enorone@gmail.com>
2015-10-07 16:18:58 +0200

    hog_similarity cosmetics

--- dataset_processing/boris_whitehog/hog_similarity.m
+++ dataset_processing/boris_whitehog/hog_similarity.m
@@ -27,16 +27,16 @@ parfor k = 1:numel(hog1)*numel(hog2)
     H1 = hog1(i).data;
     H2 = hog2(j).data;
     
-    H2p = zeros(size(H1) + [2*padsize,2*padsize,0]);
+    H2p = zeros(size(H1) + [2 * padsize,2 * padsize,0]);
     
     M = min(size(H2,1), size(H2p,1));
     N = min(size(H2,2), size(H2p,2));
     
-    H2p(floor((size(H2p,1)-M)/2)+[1:M], floor((size(H2p,2)-N)/2)+[1:N],:) = ...
-        H2(floor((size(H2,1)-M)/2)+[1:M], floor((size(H2,2)-N)/2)+[1:N],:);
+    H2p(floor((size(H2p, 1) - M) / 2) + [1:M], floor((size(H2p, 2) - N) / 2) + [1:N], :) = ...
+      H2(floor((size(H2, 1) - M) / 2) + [1:M], floor((size(H2, 2) - N) / 2) + [1:N], :);
     
     % compute the convolution
-    C = convn(H2p, H1(end:-1:1,end:-1:1,end:-1:1), 'valid');
+    C = convn(H2p, H1(end:-1:1, end:-1:1, end:-1:1), 'valid');
     
     val1(k) = max(C(:));
     [I1(k), J1(k)] = find(C==max(C(:)), 1);
@@ -44,13 +44,13 @@ parfor k = 1:numel(hog1)*numel(hog2)
     % everything the same, now with the flipped descriptor
     H2 = H2(:,end:-1:1,:);
     
-    H2p(floor((size(H2p,1)-M)/2)+[1:M], floor((size(H2p,2)-N)/2)+[1:N],:) = ...
-        H2(floor((size(H2,1)-M)/2)+[1:M], floor((size(H2,2)-N)/2)+[1:N],:);
+    H2p(floor((size(H2p, 1) - M) / 2) + [1:M], floor((size(H2p, 2) - N) / 2) + [1:N], :) = ...
+      H2(floor((size(H2, 1) - M) / 2) + [1:M], floor((size(H2, 2) - N) / 2) + [1:N],:);
     
-    C = convn(H2p, H1(end:-1:1,end:-1:1,end:-1:1), 'valid');
+    C = convn(H2p, H1(end:-1:1, end:-1:1, end:-1:1), 'valid');
     
     val2(k) = max(C(:));
-    [I2(k), J2(k)] = find(C==max(C(:)), 1);
+    [I2(k), J2(k)] = find(C == max(C(:)), 1);
         
 end
 
