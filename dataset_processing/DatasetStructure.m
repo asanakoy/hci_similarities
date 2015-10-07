8ad3011249eabd14a07806c1910ddd5cae86caeb
Artsiom Sanakoyeu <enorone@gmail.com>
2015-10-07 14:59:29 +0200

    Added DatasetStructure class

---
+++ dataset_processing/DatasetStructure.m
@@ -0,0 +1,13 @@
+classdef DatasetStructure
+    properties (Constant)   
+        CLIPS_DIR        = 'clips';
+        BBOXES_DIR       = 'boxes';
+        FRAMES_DIR       = 'frames';
+        CROPS_DIR        = 'crops';
+        WHITEHOG_DIR     = 'whitehog';
+        PAIRWISE_SIM_DIR = 'pairwise_sim';
+        SIM_DIR          = 'similarities';
+        DATA_DIR         = 'data';
+        WHITEHOG_TILED_DIR = 'whitehog_tiled';
+    end
+end
