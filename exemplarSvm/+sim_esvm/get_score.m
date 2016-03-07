c5fd4a1b75257bec0cab2d5f0763ba8d0adab632
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2016-03-07 14:40:02 +0100

    get_score update

--- exemplarSvm/+sim_esvm/get_score.m
+++ exemplarSvm/+sim_esvm/get_score.m
@@ -1,6 +1,8 @@
 function [score] = get_score( I, model, detect_params )
 % Apply ESVM to the image and get score
 
+assert(length(model) == 1);
+
 if (~exist('detect_params', 'var'))
     detect_params = sim_esvm.get_default_params;
 end
@@ -13,6 +15,15 @@ detect_params.detect_pyramid_padding = 0; % size of the window shifting
 detect_params.detect_keep_threshold = -1e9; % keep all detections
 
 if strcmp(detect_params.features_type, 'HOG-like')
+    fprintf('Detection padding: %d\n', detect_params.detect_pyramid_padding);
+else
+    fprintf('Plain features.\n');
+end
+
+if strcmp(detect_params.features_type, 'HOG-like') && detect_params.should_load_features_from_disk == 0
+    assert(isnumeric(I), 'I is not an image!');
+    assert(size(I, 3) == 3, 'I is not a 3-channel Image!');
+    
     I = im2double(I); % because ESVM lib deals with double images
 else
     I = convert_to_image_struct(I, detect_params);
