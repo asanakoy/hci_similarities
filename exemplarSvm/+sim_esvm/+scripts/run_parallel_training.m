9952dfa588854bd407c76c96c62595815961921e
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2016-03-10 20:24:58 +0100

    check ' if model empty' changed

--- exemplarSvm/+sim_esvm/+scripts/run_parallel_training.m
+++ exemplarSvm/+sim_esvm/+scripts/run_parallel_training.m
@@ -31,10 +31,12 @@ parfor i = 1:length(anchor_global_ids)
     
     output_dir = fullfile(ESVM_MODELS_DIR, model_name);
     
-    if (~isempty(getFilesInDir(output_dir, '.*svm-removed_top_hrd\.mat')) || ...
+    has_final_model_file = ~isempty(getFilesInDir([output_dir, '/models'], '.*-svm\.mat'));
+    
+    if (has_final_model_file || ...
          any(find(ismember(previously_trained.trained_model_names, model_name))))
         continue;
-    elseif exist(output_dir, 'dir') && isempty(getFilesInDir(output_dir, '.*svm-removed_top_hrd\.mat'))
+    elseif exist(output_dir, 'dir') && ~has_final_model_file
         rmdir(output_dir, 's');
     end
     
